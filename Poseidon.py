166d9031391ca056db86a4ebac2313d84130fcce
MercenaryHapy6 <corazoneduardo@att.net>
2022-06-14 12:53:12 -0500

    more thinking

--- Poseidon.py
+++ Poseidon.py
@@ -1,6 +1,15 @@
-#By: Eduardo Corazon
+# By: Eduardo Corazon
 #Date: 5/31/2022
-#Description: This is a 
+#Description: This is a
 #
 
 #!/usr/bin/python
+
+# imports
+import os
+import threading
+import requests
+
+
+class Apicall():
+    th
