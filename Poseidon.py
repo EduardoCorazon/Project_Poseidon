087f7001b76a09d1e9edbae161f24421d984d92d
MercenaryHapy6 <corazoneduardo@att.net>
2022-06-16 13:42:01 -0500

    test again

--- Poseidon.py
+++ Poseidon.py
@@ -17,4 +17,6 @@ url = 'https://www.google.com/'
 driver.get(url)
 
 driver.find_element_by_xpath(
-    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('test')
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('This is a test to see speed')
+
+# driver.find_element_by_xpath('').send_keys('')
